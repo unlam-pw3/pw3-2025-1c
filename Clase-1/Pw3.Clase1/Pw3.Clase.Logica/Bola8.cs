359e46167cfb5756f1811c2f9640d9fd1dd3820a
leandrojl <leandroracing96@hotmail.com>
2025-04-13 14:31:06 -0300

    bola 8 -leandrojl

---
+++ Clase-1/Pw3.Clase1/Pw3.Clase.Logica/Bola8.cs
@@ -0,0 +1,34 @@
+﻿namespace Pw3.Clase.Logica;
+
+public class Bola8
+{
+    
+    public Bola8() { 
+        
+    }
+
+    public string obtenerRespuesta()
+    {
+        Random rnd = new Random();
+
+        string[] respuestas = new string[]
+         {
+            "Si",
+            "No",
+            "Quizas",
+            "Definitivamente",
+            "No lo creo",
+            "Tal vez",
+            "Es probable",
+            "No es probable",
+            "Nunca!"
+         };
+
+        int _numeroGenerado = rnd.Next(respuestas.Length);
+
+        return respuestas[_numeroGenerado];
+
+
+    }
+
+}
