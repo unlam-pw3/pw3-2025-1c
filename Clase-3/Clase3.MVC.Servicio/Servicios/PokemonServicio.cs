f5a6e6af24d48c55b4b5877167aba1075ba5545c
lucasebarreiro <lucasbarreiro1@outlook.com>
2025-04-26 19:30:00 -0300

    Resolucion clase 3

---
+++ Clase-3/Clase3.MVC.Servicio/Servicios/PokemonServicio.cs
@@ -0,0 +1,30 @@
+﻿using Clase3.MVC.Entidades;
+using Clase3.MVC.Servicio.Interfaces;
+
+namespace Clase3.MVC.Servicio.Servicios
+{
+    public class PokemonServicio : IPokemonService
+    {
+        private static List<Pokemon> pokemons { get; set; }
+
+        public PokemonServicio()
+        {
+            if (pokemons == null)
+            {
+                pokemons = new List<Pokemon>();
+                pokemons.Add(new Pokemon { Id = 1, Nombre = "Bulbasaur", Tipo = "Planta - Veneno", Imagen = "/imagenes/bulbasaur.png" });
+                pokemons.Add(new Pokemon { Id = 2, Nombre = "Ivysaur", Tipo = "Planta - Veneno", Imagen = "/imagenes/ivysaur.png" });
+                pokemons.Add(new Pokemon { Id = 3, Nombre = "Venusaur", Tipo = "Planta - Veneno", Imagen = "/imagenes/venusaur.png" });
+            }
+        }
+        public void AgregarPokemon(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+        }
+
+        public List<Pokemon> ObtenerPokemons()
+        {
+            return pokemons;
+        }
+    }
+}
---
+++ Clase-3/Clase3.MVC.WebApp/Controllers/PokemonController.cs
@@ -0,0 +1,53 @@
+﻿using Clase3.MVC.Entidades;
+using Clase3.MVC.Servicio.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase3.MVC.WebApp.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonService _pokemonService;
+
+        public PokemonController(IPokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+        }
+        public IActionResult Index()
+        {
+            return View(_pokemonService.ObtenerPokemons());
+        }
+
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Agregar(Pokemon pokemon, IFormFile imagen)
+        {
+            if (imagen != null && imagen.Length > 0)
+            {
+                var rutaCarpeta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "imagenes");
+
+                if (!Directory.Exists(rutaCarpeta))
+                {
+                    Directory.CreateDirectory(rutaCarpeta);
+                }
+
+                var nombreArchivo = $"{pokemon.Nombre}{Path.GetExtension(imagen.FileName)}";
+                var rutaCompleta = Path.Combine(rutaCarpeta, nombreArchivo);
+
+                using (var stream = new FileStream(rutaCompleta, FileMode.Create))
+                {
+                    await imagen.CopyToAsync(stream);
+                }
+
+                pokemon.Imagen = $"/imagenes/{nombreArchivo}";
+            }
+            _pokemonService.AgregarPokemon(pokemon);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
