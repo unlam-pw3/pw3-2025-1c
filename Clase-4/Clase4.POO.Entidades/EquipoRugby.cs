299331b2eaffe145de4fbf18f1fc6a43d86c5139
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2025-04-30 20:53:30 -0300

    Agregando ejemplo Clase 4 POO. Polimorfismo usando IEquipoDeportivo y Cantidad de Integrantes dependiendo del deporte

---
+++ Clase-4/Clase4.POO.Entidades/EquipoRugby.cs
@@ -0,0 +1,7 @@
+﻿namespace Clase4.POO.Entidades;
+
+public class EquipoRugby : IEquipoDeportivo
+{
+    public string Nombre { get; set; }
+    public int CantidadDeIntegrantesRequeridos() => 15;
+}
---
+++ Clase-4/Clase4.POO.Web/Controllers/EquiposController.cs
@@ -0,0 +1,24 @@
+﻿using Clase4.POO.Logica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase4.POO.Web.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly IEquiposLogica _equiposLogica;
+        public EquiposController(IEquiposLogica equiposLogica)
+        {
+            _equiposLogica = equiposLogica;
+        }
+        // GET: Equipos
+        public IActionResult Listar()
+        {
+            var equipos = _equiposLogica.ObtenerEquipos();
+            return View(equipos);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
