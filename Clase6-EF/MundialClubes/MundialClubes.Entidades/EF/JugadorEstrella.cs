077ecf2dfa82c535de69225c3320d18c4f2a830e
Lautaro Gerez <101670754+lautigrz@users.noreply.github.com>
2025-05-28 19:37:43 -0300

    tarea clase 6 (#25)

---
+++ Clase6-EF/MundialClubes/MundialClubes.Entidades/EF/JugadorEstrella.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MundialClubes.Entidades.EF;
+
+public partial class JugadorEstrella
+{
+    public int IdJugadorEstrella { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int Edad { get; set; }
+
+    public string? FotoUrl { get; set; }
+
+    public string? Descripcion { get; set; }
+}
