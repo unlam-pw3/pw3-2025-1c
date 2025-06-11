71713e2a344b36cc362dc55897f39667a130b25f
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2025-06-11 20:54:15 -0300

    agregando modelo 2 parcial Gestion Reservas

---
+++ Clase-9-Modelo-2do-Parcial/GestionReservas/GestionReservas.Data/EF/Destino.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionReservas.Data.EF;
+
+public partial class Destino
+{
+    public int IdDestino { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
+}
---
+++ Clase-9-Modelo-2do-Parcial/GestionReservas/GestionReservas.Logica/DestinoLogica.cs
@@ -0,0 +1,24 @@
+﻿using GestionReservas.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionReservas.Logica;
+
+public interface IDestinoLogica
+{
+    Task<List<Destino>> ObtenerDestinosAsync();
+}
+
+public class DestinoLogica: IDestinoLogica
+{
+    private readonly _20251cModel2doPGestionReservasContext _context;
+    public DestinoLogica(_20251cModel2doPGestionReservasContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Destino>> ObtenerDestinosAsync()
+    {
+        return await _context.Destinos
+                .OrderBy(d=> d.Nombre)
+                .ToListAsync();
+    }
+}
